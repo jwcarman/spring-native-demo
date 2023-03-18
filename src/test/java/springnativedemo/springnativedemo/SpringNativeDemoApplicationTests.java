74a64f2396538235611e17aeb5bfcb3238e52d31
James Carman <jcarman@callibrity.com>
2023-03-18 10:00:03 -0400

    Initial commit

---
+++ src/test/java/springnativedemo/springnativedemo/SpringNativeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springnativedemo.springnativedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
